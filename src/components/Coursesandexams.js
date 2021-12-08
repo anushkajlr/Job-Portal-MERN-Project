b74164714de9d50e15a9596f067d580ab0b19808
Arushi2002 <79096912+Arushi2002@users.noreply.github.com>
2021-12-08 10:12:20 +0530

    Update Coursesandexams.js

--- src/components/Coursesandexams.js
+++ src/components/Coursesandexams.js
@@ -5,39 +5,39 @@ const CoursesandExams = () => {
         <div class="col s12"></div>
         <div style = {{fontSize:"25px"}}class="col s4">
         <h3>Computer IT Courses</h3>
-                        <a href="https://www.freshersworld.com/big-data-hadoop-training-institutes/4343020020" target="_blank">Big Data Hadoop</a><br/><br/>
-                        <a href="https://www.freshersworld.com/networking-ccna-training-institutes/4343020023" target="_blank">Networking and CNN</a><br/><br/>
-                        <a href="https://www.freshersworld.com/c-c++-training-institutes/4343020021" target="_blank">C C++</a><br/><br/>
-                        <a href="https://www.freshersworld.com/cloud-computing-training-institutes/4343020024" target="_blank">Cloud Computing</a><br/><br/>
-                        <a href="https://www.freshersworld.com/mysql-database-training-institutes/4343020034" target="_blank">My SQL/Database</a><br/><br/>
-                        <a href="https://www.freshersworld.com/app-development-android-ios-widows-training-institutes/4343020019" target="_blank">App Developement</a><br/><br/>
-                        <a href="https://www.freshersworld.com/web-designing-training-institutes/4343020045" target="_blank">Web Designing</a><br/><br/>
-                        <a href="https://www.freshersworld.com/java-j2ee-training-institutes/4343020031" target="_blank">Java/J2EE</a><br/><br/>
+                        <a href="https://www.freshersworld.com/big-data-hadoop-training-institutes/4343020020" target="_blank" rel="noreferrer">Big Data Hadoop</a><br/><br/>
+                        <a href="https://www.freshersworld.com/networking-ccna-training-institutes/4343020023" target="_blank" rel="noreferrer">Networking and CNN</a><br/><br/>
+                        <a href="https://www.freshersworld.com/c-c++-training-institutes/4343020021" target="_blank" rel="noreferrer">C C++</a><br/><br/>
+                        <a href="https://www.freshersworld.com/cloud-computing-training-institutes/4343020024" target="_blank" rel="noreferrer">Cloud Computing</a><br/><br/>
+                        <a href="https://www.freshersworld.com/mysql-database-training-institutes/4343020034" target="_blank" rel="noreferrer">My SQL/Database</a><br/><br/>
+                        <a href="https://www.freshersworld.com/app-development-android-ios-widows-training-institutes/4343020019" target="_blank" rel="noreferrer">App Developement</a><br/><br/>
+                        <a href="https://www.freshersworld.com/web-designing-training-institutes/4343020045" target="_blank" rel="noreferrer">Web Designing</a><br/><br/>
+                        <a href="https://www.freshersworld.com/java-j2ee-training-institutes/4343020031" target="_blank" rel="noreferrer">Java/J2EE</a><br/><br/>
                     
         </div>
         <div style = {{fontSize:"25px"}} class="col s4"><h3>Study Aboard</h3>
-                        <a href="https://studyabroad.shiksha.com/mba-in-abroad-dc11508" target="_blank">MBA</a><br/><br/>
-                        <a href="https://studyabroad.shiksha.com/ms-in-abroad-dc11509" target="_blank">MS</a><br/><br/>
-                        <a href="https://studyabroad.shiksha.com/sop-statement-of-purpose-applycontent1701" target="_blank">SOP</a><br/><br/>
-                        <a href="https://studyabroad.shiksha.com/exams/gmat" target="_blank">GMAT</a><br/><br/>
-                        <a href="https://www.shiksha.com/mba/cat-exam" target="_blank">IELTS</a><br/><br/><br/><br/><br/><br/>
+                        <a href="https://studyabroad.shiksha.com/mba-in-abroad-dc11508" target="_blank" rel="noreferrer">MBA</a><br/><br/>
+                        <a href="https://studyabroad.shiksha.com/ms-in-abroad-dc11509" target="_blank" rel="noreferrer">MS</a><br/><br/>
+                        <a href="https://studyabroad.shiksha.com/sop-statement-of-purpose-applycontent1701" target="_blank" rel="noreferrer">SOP</a><br/><br/>
+                        <a href="https://studyabroad.shiksha.com/exams/gmat" target="_blank" rel="noreferrer">GMAT</a><br/><br/>
+                        <a href="https://www.shiksha.com/mba/cat-exam" target="_blank" rel="noreferrer">IELTS</a><br/><br/><br/><br/><br/><br/>
                         <h3>Interview Advice</h3>
-                        <a href="https://www.naukri.com/blog/tag/interview-advice/" target="_blank">Advice</a><br/><br/>
+                        <a href="https://www.naukri.com/blog/tag/interview-advice/" target="_blank" rel="noreferrer">Advice</a><br/><br/>
             </div>
         <div style = {{fontSize:"25px"}} class="col s4"><h3>Top Exams</h3>
-                        <a href="https://www.shiksha.com/b-tech/jee-main-exam" target="_blank">JEE Mains</a><br/><br/>
-                        <a href="https://www.shiksha.com/b-tech/jee-advanced-exam" target="_blank">JEE Advanced</a><br/><br/>
-                        <a href="https://www.shiksha.com/science/bcece-exam" target="_blank">BCECE</a><br/><br/>
-                        <a href="https://www.shiksha.com/engineering/gate-exam" target="_blank">GATE</a><br/><br/>
-                        <a href="https://www.shiksha.com/mba/cat-exam" target="_blank">CAT</a><br/><br/>
-                        <a href="https://www.shiksha.com/mba/snap-exam" target="_blank">SNAP</a><br/><br/>
-                        <a href="https://www.shiksha.com/mba/mat-exam" target="_blank">MAT</a><br/><br/>
-                        <a href="https://www.shiksha.com/medicine-health-sciences/neet-exam" target="_blank">NEET</a><br/><br/>
-                        <a href="https://www.shiksha.com/exams/sbi-po-exam" target="_blank">SBI PO</a><br/><br/>
-                        <a href="https://www.shiksha.com/exams/ibps-po-exam" target="_blank">IBPS PO</a><br/><br/>
-                        <a href="https://www.ets.org/gre" target="_blank">GRE</a><br/><br/>
-                        <a href="https://www.ets.org/toefl/test-takers/ibt/about" target="_blank">TOEFL</a><br/><br/>
-                        <a href="https://www.careerindia.com/upsc/ies-exam-e26.html" target="_blank">IES</a><br/><br/></div>
+                        <a href="https://www.shiksha.com/b-tech/jee-main-exam" target="_blank" rel="noreferrer">JEE Mains</a><br/><br/>
+                        <a href="https://www.shiksha.com/b-tech/jee-advanced-exam" target="_blank" rel="noreferrer">JEE Advanced</a><br/><br/>
+                        <a href="https://www.shiksha.com/science/bcece-exam" target="_blank" rel="noreferrer">BCECE</a><br/><br/>
+                        <a href="https://www.shiksha.com/engineering/gate-exam" target="_blank" rel="noreferrer">GATE</a><br/><br/>
+                        <a href="https://www.shiksha.com/mba/cat-exam" target="_blank" rel="noreferrer">CAT</a><br/><br/>
+                        <a href="https://www.shiksha.com/mba/snap-exam" target="_blank" rel="noreferrer">SNAP</a><br/><br/>
+                        <a href="https://www.shiksha.com/mba/mat-exam" target="_blank" rel="noreferrer">MAT</a><br/><br/>
+                        <a href="https://www.shiksha.com/medicine-health-sciences/neet-exam" target="_blank" rel="noreferrer">NEET</a><br/><br/>
+                        <a href="https://www.shiksha.com/exams/sbi-po-exam" target="_blank" rel="noreferrer">SBI PO</a><br/><br/>
+                        <a href="https://www.shiksha.com/exams/ibps-po-exam" target="_blank" rel="noreferrer">IBPS PO</a><br/><br/>
+                        <a href="https://www.ets.org/gre" target="_blank" rel="noreferrer">GRE</a><br/><br/>
+                        <a href="https://www.ets.org/toefl/test-takers/ibt/about" target="_blank" rel="noreferrer">TOEFL</a><br/><br/>
+                        <a href="https://www.careerindia.com/upsc/ies-exam-e26.html" target="_blank" rel="noreferrer">IES</a><br/><br/></div>
         </div>
     )
 }
