e1daef0d463cdc8fafc9cd6f5bef1ef107188afa
Arushi2002 <79096912+Arushi2002@users.noreply.github.com>
2021-12-08 10:11:33 +0530

    Update Downbar.js

--- src/components/Downbar.js
+++ src/components/Downbar.js
@@ -7,12 +7,11 @@ return (
     <a className="brand-logo">Connect with us on finger tips</a>
     <ul className="right">
     
-    <li><NavLink to="/Jobs">Insta</NavLink></li>
-    <li><NavLink to="/Coursesandexams">Fb</NavLink></li>
-    <li><NavLink to="/Connections"></NavLink>YouTube</li>
-    <li><NavLink to="/profile">LinkedIn</NavLink></li>
-    <li><NavLink to="/login">Twitter</NavLink></li>
-    <li><NavLink to="/Signup">G+</NavLink></li>
+    <li><a href="https://www.instagram.com/freshersworld/?utm_medium=copy_link" target="_blank" rel="noreferrer">Insta</a></li>
+    <li><a href="https://www.facebook.com/freshersworld"  target="_blank" rel="noreferrer">Fb</a></li>
+    <li><a href="https://www.youtube.com/freshersworld" target="_blank" rel="noreferrer">YouTube</a></li>
+    <li><a href="https://www.linkedin.com/company/freshersworld-com/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+    <li><a href="https://twitter.com/freshersworld" target="_blank" rel="noreferrer">Twitter</a></li>
     </ul>
     </div>
 </nav>
